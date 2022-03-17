e8e012826c7902b11a03de7bdf4a1ba08dbe0ca0
Andrew Mironov <mrvandrey@gmail.com>
2022-03-17 17:35:24 +0400

    1. Динамический список на массиве. [#158]

--- src/main/java/ru/job4j/collection/list/SimpleArrayList.java
+++ src/main/java/ru/job4j/collection/list/SimpleArrayList.java
@@ -14,11 +14,16 @@ public class SimpleArrayList<T> implements List<T> {
         this.container = (T[]) new Object[capacity];
     }
 
-    @Override
-    public void add(T value) {
+    public T[] arrayDoubleLength(T[] container) {
         if (container.length == size) {
             container = Arrays.copyOf(container, container.length * 2);
         }
+        return container;
+    }
+
+    @Override
+    public void add(T value) {
+        container = arrayDoubleLength(container);
         container[size] = value;
         size++;
         modCount++;
@@ -35,6 +40,7 @@ public class SimpleArrayList<T> implements List<T> {
 
     @Override
     public T remove(int index) {
+        Objects.checkIndex(index, size);
         T oldValue = container[index];
         System.arraycopy(container, index + 1, container, index, container.length - index - 1);
         container[container.length - 1] = null;
@@ -45,7 +51,7 @@ public class SimpleArrayList<T> implements List<T> {
 
     @Override
     public T get(int index) {
-        Objects.checkIndex(index, container.length);
+        Objects.checkIndex(index, size);
         return container[index];
     }
 
@@ -65,7 +71,7 @@ public class SimpleArrayList<T> implements List<T> {
                 if (expectedModCount != modCount) {
                     throw new ConcurrentModificationException();
                 }
-                return cursor < container.length && size > 0;
+                return cursor < size && size > 0;
             }
 
             @Override
