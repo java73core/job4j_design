4692b3074f3a18d5e9d68cc7ca789dea10eb4835
Andrew Mironov <mrvandrey@gmail.com>
2022-03-28 16:18:05 +0400

    Очередь на двух стеках

--- src/main/java/ru/job4j/collection/list/SimpleQueue.java
+++ src/main/java/ru/job4j/collection/list/SimpleQueue.java
@@ -1,23 +1,29 @@
 package ru.job4j.collection.list;
 
+import java.util.NoSuchElementException;
+
 public class SimpleQueue<T> {
     private final SimpleStack<T> in = new SimpleStack<>();
     private final SimpleStack<T> out = new SimpleStack<>();
-    private int size1 = 0;
-    private int size2 = 0;
+    private int sizeIn = 0;
+    private int sizeOut = 0;
 
     /**
      * метод удаления первого элемента из стека. Первый элемент стека, он же последний добавленный.
      * @return удалённый элемент
      */
     public T poll() {
-            if (size2 == 0) {
-                while (size1 > 0) {
+        if (in == null) {
+            throw new NoSuchElementException();
+        }
+            if (sizeOut == 0) {
+                while (sizeIn > 0) {
                     out.push(in.pop());
-                    size1--; size2++;
+                    sizeIn--;
+                    sizeOut++;
                 }
             }
-            size2--;
+            sizeOut--;
             return out.pop();
     }
 
@@ -27,6 +33,6 @@ public class SimpleQueue<T> {
      */
     public void push(T value) {
         in.push(value);
-        size1++;
+        sizeIn++;
     }
 }
