01ac1037f4236290c448c8457a514dbf74d385f9
Andrew Mironov <mrvandrey@gmail.com>
2022-04-01 14:44:26 +0400

    2. Без переопределения equals и hashCode

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -1,6 +1,9 @@
 package ru.job4j.map;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class User {
     private String name;
@@ -14,5 +17,14 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    public static void main(String[] args) {
+        User user1 = new User("Ivan", 3, new GregorianCalendar(2000, 2, 25));
+        User user2 = new User("Ivan", 3, new GregorianCalendar(2000, 2, 25));
+        Map<User, Object> map = new HashMap<>();
+        map.put(user1, new Object());
+        map.put(user2, new Object());
+        System.out.println(map);
     }
 }
