81e9ae099bc3bac5cb6d4c6bbc923fc97c888c44
Andrew Mironov <mrvandrey@gmail.com>
2022-04-18 20:54:28 +0400

    1. Читаем файл конфигурации [#858]

---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,42 @@
+package ru.job4j.io;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ConfigTest {
+
+    @Test
+    public void whenPairWithValue() {
+        String path = "./data/app.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("hibernate.connection.password"), is("password"));
+    }
+
+    @Test
+    public void whenUrlWithAddress() {
+        String path = "./data/app.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("hibernate.connection.url"),
+                is("jdbc:postgresql://127.0.0.1:5432/trackstudio"));
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void whenNotCorrectPair() {
+        String path = "./data/notcorrect.properties";
+        Config config = new Config(path);
+        config.load();
+    }
+
+    @Test
+    public void whenEmptyConfigFile() {
+        String path = "./data/empty.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("hibernate.connection.url"),
+                is(Matchers.nullValue()));
+    }
+}
