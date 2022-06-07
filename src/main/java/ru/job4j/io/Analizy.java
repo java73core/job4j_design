11af187a672a8d85aab6a80d5a0775eac611c943
Andrew Mironov <mrvandrey@gmail.com>
2022-06-07 14:17:34 +0400

    1. Читаем файл конфигурации [#858]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -1,21 +1,33 @@
 package ru.job4j.io;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Analizy {
     public void unavailable(String source, String target) {
-        AtomicBoolean flag = new AtomicBoolean(false);
+        AtomicBoolean flag = new AtomicBoolean(true);
+        var ref = new Object() {
+            String time = "";
+        };
             try (BufferedReader read = new BufferedReader(new FileReader(source));
                  PrintWriter write = new PrintWriter(new FileOutputStream(target))) {
 
-read.lines().filter(s -> (!s.isEmpty()))
-            .map(s -> s.split(" ", 2))
-            .filter(s -> "400".equals(s[0]) || "500".equals(s[0]) || flag.get())
-            .forEach(s -> {
-                write.print(s[1] + "; ");
-                flag.set(true);
-            });
+            read.lines().filter(s -> (!s.isEmpty()))
+                        .map(s -> s.split(" ", 2))
+                        .forEach(s -> {
+                                 if ("200".equals(s[0]) || "300".equals(s[0])) {
+                                     if (!flag.get()) {
+                                          flag.set(true);
+                                          write.print(ref.time + "; " + s[1] + "\n");
+                                    }
+                                 } else {
+                                        if (flag.get()) {
+                                            flag.set(false);
+                                            ref.time = s[1];
+                                         }
+                                 }
+                        });
         } catch (IOException e) {
             e.printStackTrace();
         }
