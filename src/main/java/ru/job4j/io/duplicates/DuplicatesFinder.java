f0187c022dd09bced1f9ebdf743eb7451b592840
Andrew Mironov <mrvandrey@gmail.com>
2022-06-16 17:57:16 +0400

    4.2. Поиск дубликатов. Исправлены замечания

--- src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -3,12 +3,17 @@ package ru.job4j.io.duplicates;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Collection;
 
 public class DuplicatesFinder {
 
     public static void main(String[] args) throws IOException {
-        Files.walkFileTree(Path.of("C:\\test"), new DuplicatesVisitor());
         DuplicatesVisitor duplicatesVisitor = new DuplicatesVisitor();
-        duplicatesVisitor.getDoubleFile().values().stream().filter(x -> x.size() > 1).forEach(System.out::println);
+        Files.walkFileTree(Path.of("C:\\test"), duplicatesVisitor);
+        duplicatesVisitor.getDoubleFile()
+                .values().stream()
+                .filter(x -> x.size() > 1)
+                .flatMap(Collection::stream)
+                .forEach(System.out::println);
     }
 }
