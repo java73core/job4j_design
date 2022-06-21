e58892b1d9a8cf87d5ee8478d8a6efbb3d898cba
Andrew Mironov <mrvandrey@gmail.com>
2022-06-21 19:18:28 +0400

    5.2. Архивировать проект. Рефакторинг

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -1,7 +1,9 @@
 package ru.job4j.io;
 
 import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
@@ -36,9 +38,28 @@ public class Zip {
         }
     }
 
+    public static void valid(String[] args) {
+        if (args.length != 3) {
+            throw new IllegalArgumentException("Количество аргументов не соответствует требованиям задачи!");
+        }
+        if (!Files.exists(Paths.get(names.get("d")))) {
+            throw new IllegalArgumentException("Отсутствуют аргументы: Каталог поиска! ");
+        }
+        if (!new File(names.get("d")).isDirectory()) {
+            throw new IllegalArgumentException("Отсутствует каталог! ");
+        }
+        if (!names.get("e").contains(".")) {
+            throw new IllegalArgumentException("Отсутствуют аргументы: Расширение файла! ");
+        }
+        if (!names.get("o").contains(".zip")) {
+            throw new IllegalArgumentException("Отсутствуют аргументы: output - во что проходит архивация! ");
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         Zip zips = new Zip();
         names.parse(args);
+        valid(args);
         zips.packFiles(search.search(Path.of(names.get("d")), p -> p.toFile().getName().endsWith(names.get("e"))), Path.of(names.get("o")));
         Zip zip = new Zip();
         zip.packSingleFile(
