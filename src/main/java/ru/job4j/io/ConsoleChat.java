964517373a92264817bb4c80cf98f7078ffa6cbc
Andrew Mironov <mrvandrey@gmail.com>
2022-06-22 17:05:19 +0400

    Рефакторинг

--- src/main/java/ru/job4j/io/ConsoleChat.java
+++ src/main/java/ru/job4j/io/ConsoleChat.java
@@ -40,7 +40,7 @@ public class ConsoleChat {
             if (Objects.equals(input, CONTINUE) && !flagStop) {
                 flagStop = true;
             }
-           if(flagStop || flagWhile) {
+           if (flagStop || flagWhile) {
                logs.add(input + ls);
            }
             if (flagStop) {
