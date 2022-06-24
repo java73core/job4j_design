43c1ef1c09d19fe7d8e06c8b3295263e667e6d41
Andrew Mironov <mrvandrey@gmail.com>
2022-06-24 18:34:25 +0400

    4.1. Сканирование файловой системы. [#106929]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -5,6 +5,12 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
+    public static String splitter(String inputString) {
+        String[] str = inputString.split(" ");
+        String[] message = str[1].split("=");
+        return message[1];
+    }
+
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
@@ -12,16 +18,23 @@ public class EchoServer {
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
-                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                    String closed = in.readLine();
-                    if (closed.contains("Bye")) {
-                        out.write("Connection closed\r\n\r\n".getBytes());
+                   out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    String input = splitter(in.readLine());
+                    if (input.equals("Hello")) {
+                        out.write(input.getBytes());
+                        out.flush();
+                    } else
+                    if (input.equals("Exit")) {
+                        out.write(input.getBytes());
                         server.close();
+                    } else {
+                        out.write(input.getBytes());
+                        out.flush();
                     }
                     for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
                         System.out.println(str);
                     }
-                    out.flush();
+                     out.flush();
                 }
             }
         }
