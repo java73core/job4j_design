680bd6f132b6fb4e5d6d983b32d48f4f08e8402c
Andrew Mironov <mrvandrey@gmail.com>
2022-07-01 11:08:10 +0400

    0. Что такое база данных. [#6860]

--- src/main/java/ru/job4j/sql/person.sql
+++ src/main/java/ru/job4j/sql/person.sql
@@ -1,11 +1,13 @@
 create table person(
     id serial primary key,
-    name varchar,
-    surname varchar
+    name varchar(20),
+    surname varchar(30),
+    date_birth DATE,
+    weight DECIMAL(5, 2)
 );
-insert into person(name, surname) values ('Иван', 'Иванов');
-insert into person(name, surname) values ('Петр', 'Петров');
-insert into person(name, surname) values ('Пидр', 'Сидоров');
-insert into person(name, surname) values ('Вася', 'Васин');
+insert into person(name, surname, date_birth, weight) values ('Иван', 'Иванов', '2000-05-05', 75.3);
+insert into person(name, surname, date_birth, weight) values ('Петр', 'Петров', '2001-06-01', 74.3);
+insert into person(name, surname, date_birth, weight) values ('Пидр', 'Сидоров', '2002-07-02', 78.3);
+insert into person(name, surname, date_birth, weight) values ('Вася', 'Васин', '2003-07-03', 70.3);
 delete from person where name='Вася';
 update person set name = 'Cидр' where name = 'Пидр';
