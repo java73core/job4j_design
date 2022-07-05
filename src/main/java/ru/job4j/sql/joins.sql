cc9af0657c859b2cc3230c09d6c1723a9fff2504
Andrew Mironov <mrvandrey@gmail.com>
2022-07-05 13:38:18 +0400

    0. Join [#6862]

--- src/main/java/ru/job4j/sql/joins.sql
+++ src/main/java/ru/job4j/sql/joins.sql
@@ -1,75 +1,62 @@
-create table employees (
-    id serial primary key,
-    name varchar(255)
+create table teens(
+       id serial primary key,
+       name varchar(255),
+       gender varchar(2)
 );
 
 create table departments (
+       id serial primary key,
+       name varchar(255)
+);
+
+create table employees (
     id serial primary key,
     name varchar(255),
-    employees_id int  references employees(id)
+    departments_id int  references departments(id),
+    teens_id int references teens(id)
 );
 
-create table teens(
-      id serial primary key,
-      name varchar(255),
-      gender varchar(2),
-      employees_id int  references employees(id)
-);
-insert into employees(name) values ('Kate');
-insert into employees(name) values ('igor');
-insert into employees(name) values ('Olga');
-insert into employees(name) values ('lion');
-insert into employees(name) values ('Petr');
-insert into employees(name) values ('Lena');
-insert into employees(name) values ('Fedot');
-insert into employees(name) values ('Alex');
-insert into employees(name) values ('Anton');
-insert into employees(name) values ('Anna');
+insert into  departments(name) values('OOO FirstCompany');
+insert into  departments(name) values('OOO SecondCompany');
+insert into  departments(name) values('OOO EmptyCompany');
+insert into  departments(name) values('OOO SimpleCompany');
+insert into  departments(name) values('OOO Company');
 
-insert into teens(name, gender, employees_id) VALUES ('Women', 'W', 1);
-insert into teens(name, gender, employees_id) VALUES ('Men', 'M', 2);
-insert into teens(name, gender, employees_id) VALUES ('Women', 'W', 3);
-insert into teens(name, gender, employees_id) VALUES ('Men', 'M', 4);
-insert into teens(name, gender, employees_id) VALUES ('Men', 'M', 5);
-insert into teens(name, gender, employees_id) VALUES ('Women', 'W', 6);
-insert into teens(name, gender, employees_id) VALUES ('Men', 'M', 7);
-insert into teens(name, gender, employees_id) VALUES ('Men', 'M', 8);
-insert into teens(name, gender, employees_id) VALUES ('Men', 'M', 9);
-insert into teens(name, gender, employees_id) VALUES ('Women', 'W', 10);
+insert into teens(name, gender) VALUES ('Women', 'W');
+insert into teens(name, gender) VALUES ('Men', 'M');
 
-insert into  departments(name, employees_id) values('OOO FirstCompany', 1);
-insert into  departments(name, employees_id) values('OOO FirstCompany', 2);
-insert into  departments(name, employees_id) values('OOO FirstCompany', 3);
-insert into  departments(name, employees_id) values('OOO FirstCompany', 4);
-insert into  departments(name, employees_id) values('OOO FirstCompany', 5);
-insert into  departments(name, employees_id) values('OOO SecondCompany', 6);
-insert into  departments(name, employees_id) values('OOO SecondCompany', 7);
-insert into  departments(name, employees_id) values('OOO SecondCompany', 8);
-insert into  departments(name, employees_id) values('OOO SecondCompany', 9);
-insert into  departments(name, employees_id) values('OOO SecondCompany', 10);
-insert into  departments(name, employees_id) values('OOO EmptyCompany', null);
-insert into  departments(name, employees_id) values('OOO SimpleCompany', null);
-insert into  departments(name, employees_id) values('OOO Compan0y', null);
+insert into employees(name, teens_id, departments_id) values ('Kate', 1, 1);
+insert into employees(name, teens_id, departments_id) values ('igor', 2, 2);
+insert into employees(name, teens_id, departments_id) values ('Olga', 1, 2);
+insert into employees(name, teens_id, departments_id) values ('lion', 2, 1);
+insert into employees(name, teens_id, departments_id) values ('Petr', 2, 2);
+insert into employees(name, teens_id, departments_id) values ('Lena', 1, 1);
+insert into employees(name, teens_id, departments_id) values ('Fedot', 2, 2);
+insert into employees(name, teens_id, departments_id) values ('Alex', 2, 1);
+insert into employees(name, teens_id, departments_id) values ('Anton', 2, 2);
+insert into employees(name, teens_id, departments_id) values ('Anna', 1, 1);
 
-select * from employees e left join departments d on e.id = d.employees_id;
 
-select * from employees e right join departments d on e.id = d.employees_id;
 
-select * from employees e full join departments d on e.id = d.employees_id;
+select * from employees e left join departments d on e.departments_id = d.id;
+
+select * from employees e right join departments d on e.departments_id = d.id;
+
+select * from employees e full join departments d on e.departments_id = d.id;
 
 select * from employees e cross join  departments d;
 
 select  d.name Контора, e.name Имя_работника from departments d
-   left join employees e on d.employees_id = e.id
+   left join employees e on d.id = e.departments_id
 group by e.name, d.name
 having e.name is null;
 
 select  d.name Контора, e.name Имя_работника from departments d
-        left join employees e on d.employees_id = e.id
-group by e.name, d.name
+        left join employees e on d.id = e.departments_id
+group by e.name, d.name;
 
 select  d.name Контора, e.name Имя_работника from employees e
-         right join departments d on e.id = d.employees_id
+         right join departments d on d.id = e.departments_id
 group by e.name, d.name;
 
 select e.name, t.name from employees e cross join teens t;
