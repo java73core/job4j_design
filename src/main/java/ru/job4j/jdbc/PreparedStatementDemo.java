7f5e04c2080346cd6bbb6d75ce42a143158d89f7
Andrew Mironov <mrvandrey@gmail.com>
2022-07-08 17:14:37 +0400

    0.2. PreparedStatement [#379307]

---
+++ src/main/java/ru/job4j/jdbc/PreparedStatementDemo.java
@@ -0,0 +1,33 @@
+package ru.job4j.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class PreparedStatementDemo {
+
+    private Connection connection;
+
+    public PreparedStatementDemo() throws Exception {
+        initConnection();
+    }
+
+    public void initConnection() throws Exception {
+        Class.forName("org.postgresql.Driver");
+        String url = "jdbc:postgresql://localhost:5432/idea_db";
+        String login = "postgres";
+        String password = "password";
+        connection = DriverManager.getConnection(url, login, password);
+    }
+
+    public void insert(City city) {
+        try (PreparedStatement statement =
+                     connection.prepareStatement("insert into cities(name, population) values (?, ?)")) {
+            statement.setString(1, city.getName());
+            statement.setInt(2, city.getPopulation());
+            statement.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
